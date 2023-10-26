ed013dfb86d1d3b63180643288b7770f4b23c22e
SofiyaBirchova <sofity92@gmail.com>
2023-10-26 10:25:01 +0300

    Add project files.

---
+++ TestApp.UnitTests/FactorialTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.UnitTests;
+
+public class FactorialTests
+{
+    [Test]
+    public void CalculateFactorial_InputZero_ReturnsOne()
+    {
+        // Arrange
+        int input = 0;
+
+        // Act
+        int result = Factorial.CalculateFactorial(input);
+
+        // Assert
+        Assert.AreEqual(1, result);
+    }
+
+    [Test]
+    public void CalculateFactorial_InputPositiveNumber_ReturnsCorrectFactorial()
+    {
+        // Arrange
+        int input = 3;
+
+        // Act
+        int result = Factorial.CalculateFactorial(input);
+
+        // Assert
+        Assert.AreEqual(6, result);
+    }
+
+    [Test]
+    public void CalculateFactorial_InputNegativeNumber_ThrowsException()
+    {
+        // Arrange
+        int input = -1;
+
+        // Act
+        // int result = Factorial.CalculateFactorial(input);
+
+        // Assert
+        Assert.Throws<ArgumentOutOfRangeException>(() => { Factorial.CalculateFactorial(input); });
+    }
+}
---
+++ TestApp.UnitTests/StringReverseTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class StringReverseTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_Reverse_WhenGivenEmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string input = "";
+
+        // Act
+        string actual = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual("", actual);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenSingleCharacterString_ReturnsSameCharacter()
+    {
+        // Arrange
+        string input = "X";
+
+        // Act
+        string result = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual("X", result);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenNormalString_ReturnsReversedString()
+    {
+        // Arrange
+        string input = "XYZ";
+
+        // Act
+        string result = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual("ZYX", result);
+    }
+}
